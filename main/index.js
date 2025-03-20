376bf9caa97c31cea2ac3af6c73fd3af4ff5657e
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2025-03-20 14:40:58 -0300

    feat: implement product categorization based on description and brand

--- main/index.js
+++ main/index.js
@@ -20,10 +20,10 @@ const KNOWN_BRANDS = [
 function extractBrand(description) {
     for (const brand of KNOWN_BRANDS) {
         if (description.toLowerCase().includes(brand.toLowerCase())) {
-            return brand;
+            return brand
         }
     }
-    return "Desconhecida"; // Retorna "Desconhecida" se a marca não for encontrada
+    return "Desconhecida"
 }
 
 /**
@@ -62,3 +62,56 @@ function calculateSimilarity (firstString, secondString){
     return 1 - distanceBetweenString / maxLengthBetweenStrings
 }
 
+/**
+ * Categorizes products based on the similarity of their descriptions and brand.
+ * - Groups equivalent products (same product with variations in description).
+ * - Separates products with different types, brands or quantities.
+ *
+ * @param {Array<Object>} products - List of products to be categorized.
+ * Each product must have a `title` property (product description).
+ * @returns {Array<Object>} List of categories, where each category contains:
+ * - `category`: Name of the category (description of the first product in the group).
+ * - `count`: Number of products in the category.
+ * - `products`: List of products in the category.
+ */
+function categorizeProducts(products) {
+    const categories = [];
+
+    products.forEach(item => {
+        const brand = extractBrand(item.title); 
+        const normalizeDescription = normalizeDescriptionProduct(item.title);
+
+        let foundCategory = false;
+        for (const category of categories) {
+            const categoryBrand = extractBrand(category.category);
+            const normalizedCategoryName = normalizeDescriptionProduct(category.category);
+
+            if (brand === categoryBrand) {
+                const similarity = calculateSimilarity(normalizedCategoryName, normalizeDescription);
+
+                if (similarity > 0.7) { 
+                    category.count++;
+                    category.products.push({
+                        title: item.title,
+                        supermarket: item.supermarket
+                    });
+                    foundCategory = true;
+                    break;
+                }
+            }
+        }
+
+        if (!foundCategory) {
+            categories.push({
+                category: item.title,
+                count: 1,
+                products: [{
+                    title: item.title,
+                    supermarket: item.supermarket
+                }]
+            });
+        }
+    });
+
+    return categories;
+}
