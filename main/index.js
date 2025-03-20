91ceb4d00c30789e5a782027e13306aba562162b
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2025-03-20 13:49:03 -0300

    feat: add string similarity calculation using Levenshtein distance (lib - Leven)

--- main/index.js
+++ main/index.js
@@ -20,3 +20,21 @@ function normalizeDescriptionProduct (description) {
             .sort()
             .join(' ')
 }
+
+
+/**
+ * Calculates the similarity between two strings using the Levenshtein distance.
+ * The similarity is normalised to a value between 0 and 1.
+ *
+ * @param {string} firstString - The first string.
+ * @param {string} secondString- The second string.
+ * @returns {number} The similarity between the strings (0 to 1).
+ */
+
+function calculateSimilarity (firstString, secondString){
+    const maxLengthBetweenStrings = Math.max(firstString.length, secondString.length)
+    if (maxLengthBetweenStrings == 0 ) return 1
+    const distanceBetweenString = leven( firstString, secondString )
+    return 1 - distanceBetweenString / maxLengthBetweenStrings
+}
+
